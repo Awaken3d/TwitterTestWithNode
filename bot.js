3639433191da1fa46cb4f6cd46d39b9664783ef5
Nicholas Souris <nisouris@hotmail.com>
2016-11-06 16:58:42 -0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+console.log('testing');
+
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+var fs = require('fs');
+
+var stream = T.stream('statuses/filter', { track: ['#Erdogan'],language: 'en' });
+
+stream.on('tweet', function (tweet) {
+    fs.appendFile("./tweets", tweet.text+"\n", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+  console.log(tweet.text);
+});
