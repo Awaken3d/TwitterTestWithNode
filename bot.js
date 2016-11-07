934dd8e3405045af08335219686cd887bcd71506
Nicholas Souris <nisouris@hotmail.com>
2016-11-07 12:42:45 -0500

    changed hashtag

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var config = require('./config');
 var T = new Twit(config);
 var fs = require('fs');
 
-var stream = T.stream('statuses/filter', { track: ['#Erdogan'],language: 'en' });
+var stream = T.stream('statuses/filter', { track: ['#Java'],language: 'en' });
 
 stream.on('tweet', function (tweet) {
     fs.appendFile("./tweets", tweet.text+"\n", function(err) {
